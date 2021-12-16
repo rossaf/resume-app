757adc6b2907dc5a85317736ad4e020f4eaefbdc
Ross F <37255573+rossaf@users.noreply.github.com>
2021-12-16 01:05:06 -0500

    Enabled Contact Form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,74 @@
 //import e from "cors"
-import React from "react"
-import { Button, Form, Header, Icon, Input, Segment, TextArea } from "semantic-ui-react"
+import React, { useEffect, useState } from "react"
+import { Button, Form, Header, Icon, Input, Segment, TextArea, Message } from "semantic-ui-react"
 import Trans from "./Trans.js"
 
 export default function Projects({theme}) {
+    const [ success, setSuccess ] = useState("")
+    const [ loading, setLoading ] = useState(false)
+    const [ validation, setValidation ] = useState([])
+    const [ fname, setFname ] = useState("")
+    const [ lname, setLname] = useState("")
+    const [ message, setMessage ] = useState("")
+    const [ email, setEmail ] = useState("")
+
+    const emailregex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+    function handleFormSubmit(e) {
+        e.preventDefault()
+        setValidation([])
+        setSuccess(false)
+        setLoading(true)
+
+        if (!fname) {
+            setValidation(validation => [...validation, "Please enter a first name."]);
+        }
+        if (!lname) {
+            setValidation(validation => [...validation, "Please enter a last name."]);      
+        }
+        if (!message) {
+            setValidation(validation => [...validation, "Please enter a message."]);       
+        }
+        if (!email) {
+            setValidation(validation => [...validation, "Please enter an email."]);       
+        }
+        if (!emailregex.test(email)) {
+            setValidation(validation => [...validation, "Your email is invalid."]);
+            setLoading(false)
+            return     
+        }
+        if (!fname || !lname || !message || !email) {
+            setLoading(false)
+            return
+        }
+
+        fetch("http://localhost:5000/contact/add",
+        {
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            method: "POST",
+            body: JSON.stringify({fname: fname, lname: lname, message: message, email: email})
+        })
+        .then(response => response.json())
+        .then(data => setSuccess(data))
+        .catch(err => {
+            setValidation(err)
+            setLoading(false)
+        })
+
+        setFname("")
+        setLname("")
+        setMessage("")
+        setEmail("")
+        setLoading(false)
+        setValidation([])
+    }
 
     return(<>
         <Trans>
             <Segment inverted={theme} padded color='teal'>
+
                 <Header as='h5'>
                     <Icon name='phone' />
                     (201)-903-2265
@@ -29,17 +90,31 @@ export default function Projects({theme}) {
         </Trans>
         <Trans>
             <Segment inverted={theme} padded>
-                <Form inverted={theme} >
+                {validation.length>0 && <Message
+                    error
+                    header='You have some errors in your submission'
+                    list={validation}
+                    />}
+                {success && <Message
+                    success
+                    header={success}
+                    />}
+                <Form inverted={theme} onSubmit={handleFormSubmit} loading={loading} >
+                    
                     <Form.Group widths='equal' >
                     <Form.Field
-                        disabled
+                        error={!fname && validation.length>0 ? true : false}
+                        onChange={(e) => setFname(e.target.value)}
+                        value={fname}
                         id='form-input-control-first-name'
                         control={Input}
                         label='First name'
                         placeholder='First name'
                     />
                     <Form.Field
-                        disabled
+                        error={!lname && validation.length>0 ? true : false}
+                        onChange={(e) => setLname(e.target.value)}
+                        value={lname}
                         id='form-input-control-last-name'
                         control={Input}
                         label='Last name'
@@ -47,26 +122,24 @@ export default function Projects({theme}) {
                     />
                     </Form.Group>
                     <Form.Field
-                    disabled
+                    error={!message && validation.length>0 ? true : false}
+                    onChange={(e) => setMessage(e.target.value)}
+                    value={message}
                     id='form-textarea-control-opinion'
                     label='Message'
                     control={TextArea}
                     placeholder='A short summary for why you are reaching out...'
                     />
                     <Form.Field
-                    disabled
+                    error={!email && validation.length>0 ? true : false}
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
                     id='form-input-control-error-email'
                     control={Input}
                     label='Email'
                     placeholder='your@email.com'
-                    error
                     />
-                    <Form.Field
-                    disabled
-                    id='form-button-control-public'
-                    control={Button}
-                    content='Confirm'
-                    />
+                    <Form.Button content='Submit' />
                 </Form>
             </Segment>
         </Trans>
